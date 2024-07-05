8ceefa5eb01db6b4c3e8e62827a032abf1bf27db
PrateekTechmatys <prateeky562@gmail.com>
2024-07-05 21:45:56 +0530

    makingRandom

--- Day4_RandomAPI/index.js
+++ Day4_RandomAPI/index.js
@@ -4,12 +4,7 @@ import { dbConnection } from "./config/database.js";
 
 
 
-dbConnection()
-.then(()=>{
     app.listen(3000,(req,res)=>{
      console.log("server is running at 3000")
     })
- }).catch((e)=>{
-     console.error("Error connecting to database", e);
-     process.exit(1);
- })
+ 
